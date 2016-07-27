ea554898963223c67a123aafedc384fa82d35d5b
Tiago <tms.martins+git@gmail.com>
2016-07-27 16:24:18 +0200

    troubleshot lifecycle and constructor

--- src/Mustafas.js
+++ src/Mustafas.js
@@ -1,19 +1,47 @@
 import { default as Wegbier } from '../node_modules/wegbier/dist/Wegbier.js';
+import { default as fUtils } from './fUtils/index.js';
+import { default as utils } from './utils.js';
+
+let defaults = {
+  config: {
+    // main container for defining the boundaries of the scrollable area and
+    // setting the event listeners. is expected to be a simple DOM node
+    container: null,
+
+    // TODO remove, the scrollable should be a list of items
+    moveable: null,
+
+    // decide what axis to allow scrolling on, gets translated into an array by
+    // the class constructor
+    axis: 'y',
+
+    // lock movement in one direction. relevant if more touch/scroll libraries
+    // are at the same spot and only the locked element should move
+    lock: false
+  },
+
+  private: {
+    wegbier: null,
+    boundHandlers: {},
+    axis: ['x', 'y']
+  }
+};
 
 export default class Mustafas {
   constructor(config) {
-    this._private = {
-      container: config.container,
-      scrollable: config.scrollable
-    };
+    this._config = fUtils.cloneDeep(defaults.config);
+    this._private = fUtils.cloneDeep(defaults.private);
 
-    // TODO should make a member _config and modify that one
-    config.moveable = {
-      width: this._private.scrollable.clientWidth,
-      height: this._private.scrollable.clientHeight
-    };
+    // NOTE there should always be a config, otherwise who passes the container?
+    if (config) fUtils.mergeDeep(this._config, config);
 
-    this._private.wegbier = new Wegbier(config);
+    let configWegbier = fUtils.cloneDeep(defaults.config);
+    // NOTE there should always be a config, otherwise who passes the container?
+    if (config) fUtils.mergeDeep(configWegbier, this._config);
+    configWegbier.moveable = this._calculateMoveableSize();
+
+    console.debug(configWegbier);
+    this._private.wegbier = new Wegbier(configWegbier);
 
     this._bindEvents();
   }
@@ -24,8 +52,8 @@ export default class Mustafas {
   destroy() {
     this._unbindEvents();
     this._private.wegbier.destroy();
-    this._private.container = null;
-    this._private.scrollable = null;
+    this._config.container = null;
+    this._config.moveable = null;
   };
 
   // LIFECYCLE
@@ -33,7 +61,7 @@ export default class Mustafas {
 
   _bindEvents() {
     this._private.boundHandlers = {
-      positionChanged: this._onPositionChanged.bind(this)
+      'wegbier:positionChanged': this._onPositionChanged.bind(this)
     };
 
     fUtils.forEach(this._private.boundHandlers, (handler, event) => {
@@ -44,13 +72,24 @@ export default class Mustafas {
 
   _unbindEvents() {
     fUtils.forEach(this._private.boundHandlers, (handler, event) => {
-      this._private.scrollable.removeEventListener(event, handler);
+      this._private.wegbier.removeEventListener(event, handler);
     });
   }
 
 
   _onPositionChanged(event) {
-    console.log("moveable pos: (" + event.detail.x + "," + event.detail.y + ")");
-    this._private.scrollable.style.webkitTransform = 'translate3d(' + event.detail.x + 'px, ' + event.detail.y + 'px, 0px)';
+    // console.log("moveable pos: (" + event.detail.x + "," + event.detail.y + ")");
+    this._config.moveable.style.webkitTransform = 'translate3d(' + event.detail.x + 'px, ' + event.detail.y + 'px, 0px)';
   }
+
+
+  // MOVEABLES
+
+
+    _calculateMoveableSize() {
+      return {
+        width: this._config.moveable.clientWidth,
+        height: this._config.moveable.clientHeight
+      };
+    }
 };
